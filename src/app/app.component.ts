7827448bff0f9e4119804a98a620a96152a8a7e9
devomohamed <mohammed.sabry633@gmail.com>
2023-08-26 20:41:58 +0300

    update v-0.0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PromotionAdsService } from './services/promotion-ads.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs';
+  constructor(private promoAds:PromotionAdsService) { }
+  ngOnInit(): void {
+    let observer = {
+      next: (value:string) => console.log(value),
+      error: (error:string) => console.log(error),
+      complete: () => console.log('complete')
+    }
+    this.promoAds.getScheduledAds(5).subscribe(observer)
+
+  }
 }
---
+++ src/app/services/promotion-ads.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PromotionAdsService {
+  private adsList:string[] = [];
+  constructor() {
+    this.adsList = [
+      'Big Discount',
+      'sale up to 50%',
+      'check our white friday offers',
+      // '',
+      'special white friday offers up to 70%'
+    ]
+  }
+
+  getScheduledAds(intervalSeconds:number):Observable<string>{
+    let addsObservable = new Observable<string>((observer)=>{
+      // observer.next()
+      // observer.error()
+      // observer.complete()
+      let counter = 0
+      let adsTimer = setInterval(()=>{
+        if (counter == this.adsList.length){
+          observer.complete()
+          // clearInterval(adsTimer)
+        }
+        if(this.adsList[counter] == ''){
+          observer.error('error: no ads found')
+        }
+        observer.next(this.adsList[counter])
+        counter++
+      }, intervalSeconds*1000)
+    })
+    return addsObservable
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
